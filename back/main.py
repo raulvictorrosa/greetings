a4686d66b9f3632e2ec2601df46b25be2fab2fa7
Raul Rosa <raulvictorrosa@gmail.com>
2021-03-27 00:24:22 -0300

    feat: change endpoint call name

--- back/main.py
+++ back/main.py
@@ -2,13 +2,13 @@ import hug
 from hug_middleware_cors import CORSMiddleware
 from datetime import datetime, time
 
-def is_time_between(begin_time, end_time, check_time=None):
+def is_time_between(begin_time, end_time, check_time = None):
   # If check time is not given, default to current UTC time
   check_time = check_time or datetime.now().time()
   if begin_time < end_time:
-      return check_time >= begin_time and check_time <= end_time
+    return check_time >= begin_time and check_time <= end_time
   else: # crosses midnight
-      return check_time >= begin_time or check_time <= end_time
+    return check_time >= begin_time or check_time <= end_time
 
 api = hug.API(__name__)
 api.http.add_middleware(hug.middleware.CORSMiddleware(api, max_age=10))
@@ -17,27 +17,28 @@ api.http.add_middleware(hug.middleware.CORSMiddleware(api, max_age=10))
 def home():
   return "Welcome"
 
-@hug.get('/greeting')
+@hug.get('/saudacao')
 def greeting():
   """Verify what is the current time and return a message to every part of the day."""
   message = ""
   phaseDay = ""
-  if is_time_between(time(00,00), time(5,59)):
+  color = ""
+  if is_time_between(time(00, 00), time(5, 59)):
     message = "Boa Madrugada"
     phaseDay = "dawn"
-    color: "#616161"
+    color = "#616161"
 
-  if is_time_between(time(6,00), time(11,59)):
+  if is_time_between(time(6, 00), time(11, 59)):
     message = "Bom Dia"
     phaseDay = "day"
-    color: "#ffca28"
+    color = "#ffca28"
 
-  if is_time_between(time(12,00), time(17,59)):
+  if is_time_between(time(12, 00), time(17, 59)):
     message = "Boa Tarde"
     phaseDay = "afternoon"
-    color: "#f57c00"
+    color = "#f57c00"
 
-  if is_time_between(time(18,00), time(23,59)):
+  if is_time_between(time(18, 00), time(23, 59)):
     message = "Boa noite"
     phaseDay = "night"
     color = "#616161"
